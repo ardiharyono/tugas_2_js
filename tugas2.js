68c6d0fa14461cda627fc236ebd0baa5beb9aae0
Ardi Haryono <ardiharyono26@gmail.com>
2021-10-16 17:08:25 +0700

    Tugas 2 JS
    Tugas ini kami kumpulkan untuk Niomic

---
+++ tugas2.js
@@ -0,0 +1,20 @@
+var number = 100;
+
+var string = "Hallo";
+
+var boolean = false;
+var nilai = 10;
+if (boolean<7)
+  boolean = true;
+  else
+  boolean = false;
+
+var nol;
+
+var array = [1,3,5,7,9]
+
+console.log(number);
+console.log(string);
+console.log(boolean);
+console.log(nol);
+console.log(array[2]);
